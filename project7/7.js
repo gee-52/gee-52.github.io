4ae9cccfc92418e9f20ac4b56738562bf0aea6e1
gee-52 <jenya.kulakova7@gmail.com>
2024-10-23 18:01:40 +0300

    7.js

--- project7/7.js
+++ project7/7.js
@@ -1,16 +1,16 @@
 $(".responsive").slick({
     dots: true,
     infinite: true,
-    speed: 400,
-    slidesToShow: 3,
-    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 744,
         settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            slidesToShow: 1
         }
       }
-    ]
+    ],
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    speed: 400
   });
